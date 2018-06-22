523ad8e93409539f62f27f0c888f0ec4a9d0536e
paulo1er <paulmadeline@orange.fr>
2018-06-22 13:15:50 +0100

    glyphicon "advise to lock the content"

--- js/popup.js
+++ js/popup.js
@@ -1,46 +1,49 @@
-function onClickPresenter(){
-  chrome.storage.sync.set({"presenter": document.getElementById("presenter").checked}, function(){});
+var option = {
+  "presenter" : false,
+  "previewColours" : true,
+  "isLatexRender" : true,
+  "lockContent" : false,
 }
 
-function onClickPreviewColours(){
-  chrome.storage.sync.set({"previewColours": document.getElementById("previewColours").checked}, function(){});
-}
-function onClickLatexRender(){
-  chrome.storage.sync.set({"isLatexRender": document.getElementById("isLatexRender").checked}, function(){});
-}
-function onClicklockContent(){
-  chrome.storage.sync.set({"lockContent": document.getElementById("lockContent").checked}, function(){});
+function onClick(){
+  var name = this.id;
+  option[name] = this.checked;
+  chrome.storage.sync.set(option, function(){});
 }
 
 function initValues(){
-  chrome.storage.sync.get({"presenter":false, "previewColours":true,  "isLatexRender":true, "lockContent":false}, callbackGetValue);
-  document.getElementById("presenter").addEventListener("change", onClickPresenter);
-  document.getElementById("previewColours").addEventListener("change", onClickPreviewColours);
-  document.getElementById("isLatexRender").addEventListener("change", onClickLatexRender);
-  document.getElementById("lockContent").addEventListener("change", onClicklockContent);
+  chrome.storage.sync.get(option, callbackGetValue);
+
+  for (var name in option){
+    if (option.hasOwnProperty(name)) {
+      document.getElementById(name).addEventListener("change", onClick);
+    }
+  }
 
 
   chrome.storage.onChanged.addListener(function(changes, namespace) {
-    if ("presenter" in changes) {
-      document.getElementById("presenter").checked = changes.presenter.newValue;
-    };
-    if ("previewColours" in changes) {
-      document.getElementById("previewColours").checked = changes.previewColours.newValue;
-    };
-    if ("isLatexRender" in changes) {
-      document.getElementById("isLatexRender").checked = changes.isLatexRender.newValue;
-    };
-    if ("lockContent" in changes) {
-      document.getElementById("lockContent").checked = changes.lockContent.newValue;
-    };
-   });
+    for (var name in option){
+      if (option.hasOwnProperty(name)) {
+        if (name in changes) {
+          option[name] = changes[name].newValue;
+          document.getElementById(name).checked = option[name];
+        };
+      }
+    }
+  });
+
+  setInterval(function(){
+    document.getElementById("needLock").hidden = !option["isLatexRender"] || option["lockContent"];
+  },100);
 }
 
 function callbackGetValue(vals){
-  document.getElementById("presenter").checked = vals.presenter;
-  document.getElementById("previewColours").checked = vals.previewColours;
-  document.getElementById("isLatexRender").checked = vals.isLatexRender;
-  document.getElementById("lockContent").checked = vals.lockContent;
+  option=vals;
+  for (var name in option){
+    if (option.hasOwnProperty(name)) {
+      document.getElementById(name).checked = option[name];
+    }
+  }
 }
 
 document.addEventListener( "DOMContentLoaded", initValues );
