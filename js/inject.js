b005380fe5fed25e9fddf0319774f959e9e2dc08
paulo1er <paulmadeline@orange.fr>
2018-07-30 09:41:54 +0100

    try, catch for urlNext and urlNext

--- js/inject.js
+++ js/inject.js
@@ -1,47 +1,48 @@
-function updateUrlSibling(){
-  var urlPrevious="";
-  var urlNext="";
-  var selected = project_tree.getProjectReferenceFromDomProject(selectOnActivePage(".selected"));
-  var previous = selected.getPreviousPotentiallyVisibleSibling();
-  if (previous != null){
-    var temp =  previous.projectid.split("-");
-    urlPrevious = "https://workflowy.com/#/" + temp[temp.length-1];
-  }
-  var next = selected.getNextPotentiallyVisibleSibling();
-  if (next != null){
-    var temp =  next.projectid.split("-");
-    urlNext = "https://workflowy.com/#/" + temp[temp.length-1];
-  }
-  $("[name=\'urlPrevious\']").attr("content", urlPrevious);
-  $("[name=\'urlNext\']").attr("content", urlNext);
-};
+function initURLchange(){
+  var oldURL = "";
+  var currentURL = window.location.href;
+  $("head").append($("<meta>").attr("name", "urlPrevious").attr("content", ""));
+  $("head").append($("<meta>").attr("name", "urlNext").attr("content", ""));
+  waitLoad(function(){
+    var interval = setInterval(function(){
+      try {checkURLchange(oldURL, currentURL)}
+      catch {clearInterval(interval); console.warn("previous and next siblings doesn't work")}
+    }, 1000);
+  });
+}
 
-var oldURL = "";
-var currentURL = window.location.href;
-$("head").append($("<meta>").attr("name", "urlPrevious").attr("content", ""));
-$("head").append($("<meta>").attr("name", "urlNext").attr("content", ""));
-
-function checkURLchange(){
+function checkURLchange(oldURL, currentURL){
   currentURL = window.location.href;
   if(currentURL != oldURL){
     $(".selected .content").removeClass("tex2jax_ignore").addClass("tex2jax_process");
-    updateUrlSibling();
+    var urlPrevious="";
+    var urlNext="";
+    var selected = project_tree.getProjectReferenceFromDomProject(selectOnActivePage(".selected"));
+    var previous = selected.getPreviousPotentiallyVisibleSibling();
+    if (previous != null){
+      var temp =  previous.projectid.split("-");
+      urlPrevious = "https://workflowy.com/#/" + temp[temp.length-1];
+    }
+    var next = selected.getNextPotentiallyVisibleSibling();
+    if (next != null){
+      var temp =  next.projectid.split("-");
+      urlNext = "https://workflowy.com/#/" + temp[temp.length-1];
+    }
+    $("[name=\'urlPrevious\']").attr("content", urlPrevious);
+    $("[name=\'urlNext\']").attr("content", urlNext);
     oldURL = currentURL;
   }
 }
 
 $(window).load(function() {
-  console.log(window);
-  waitLoad(function(){
-    setInterval(checkURLchange, 1000);
-  });
+  initURLchange();
   initLockUnlock();
 });
 
 function waitLoad(callback){
   if($('.selected').length) callback();
   else
-    setInterval(function(){
+    setTimeout(function(){
       waitLoad(callback);
     }, 1000);
 }
